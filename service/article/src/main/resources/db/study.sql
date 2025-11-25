87aad7e992578c3237489f27e83e50e7523e36d5
hyeonggyu-kim <shokoku0612@gmail.com>
2025-11-25 20:25:36 +0900

    feat: add additional SQL queries for article retrieval with various limits and offsets

--- service/article/src/main/resources/db/study.sql
+++ service/article/src/main/resources/db/study.sql
@@ -16,3 +16,11 @@ explain select * from (select article_id from article where board_id = 1 order b
 explain select * from (select article_id from article where board_id = 1 order by article_id desc limit 30 offset 8999970) t left join article on t.article_id = article.article_id;
 
 explain select count(*) from (select article_id from article where board_id = 1 limit 300301) t;
+
+select * from article where board_id = 1 order by article_id desc limit 30;
+
+select * from article where board_id =1 and article_id <251596649101604283 order by article_id desc limit 30;
+
+select * from article where board_id = 1 order by article_id asc limit 1 offset 30;
+
+select * from article where board_id =1 and article_id <251594244448399371 order by article_id desc limit 30;
