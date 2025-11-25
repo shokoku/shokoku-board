f6afab6ab5306776058a2833e404cf85d7557b10
hyeonggyu-kim <shokoku0612@gmail.com>
2025-11-25 14:12:35 +0900

    feat: init project

---
+++ service/article-read/src/main/java/shokoku/board/articleread/ArticleReadApplication.java
@@ -0,0 +1,12 @@
+package shokoku.board.articleread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleReadApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ArticleReadApplication.class, args);
+  }
+}
