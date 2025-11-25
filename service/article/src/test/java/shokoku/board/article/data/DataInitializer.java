c33313846097628695f05bb30c186ca9597ca7f9
hyeonggyu-kim <shokoku0612@gmail.com>
2025-11-25 15:19:05 +0900

    feat: add data initializer for bulk article insertion and disable SQL logging

---
+++ service/article/src/test/java/shokoku/board/article/data/DataInitializer.java
@@ -0,0 +1,52 @@
+package shokoku.board.article.data;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.support.TransactionTemplate;
+import shokoku.board.article.entity.Article;
+import shokoku.board.common.snowflake.Snowflake;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+public class DataInitializer {
+
+  @PersistenceContext
+  EntityManager entityManager;
+
+  @Autowired
+  TransactionTemplate transactionTemplate;
+  Snowflake snowflake = new Snowflake();
+  CountDownLatch latch = new CountDownLatch(BULK_INSERT_SIZE);
+
+  static final int BULK_INSERT_SIZE = 2000;
+  static final int EXECUTE_COUNT = 6000;
+
+  @Test
+  void initialize() throws InterruptedException {
+    ExecutorService executorService = Executors.newFixedThreadPool(10);
+    for (int i = 0; i < EXECUTE_COUNT; i++) {
+      executorService.submit(() -> {
+        insert();
+        latch.countDown();
+        System.out.println("latch.getCount() = " + latch.getCount());
+      });
+    }
+    latch.await();
+    executorService.shutdown();
+  }
+
+  void insert() {
+    transactionTemplate.executeWithoutResult(status -> {
+      for (int i = 0; i < BULK_INSERT_SIZE; i++) {
+        Article article = Article.create(snowflake.nextId(), "title" + i, "content" + i, 1L, 1L);
+        entityManager.persist(article);
+      }
+    });
+  }
+}
