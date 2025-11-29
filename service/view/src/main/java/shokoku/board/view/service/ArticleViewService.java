d5f4413dce450922886bf46174142d00e3d16937
hyeonggyu-kim <shokoku0612@gmail.com>
2025-11-29 16:54:42 +0900

    feat: implementation of view count abuse prevention policy

--- service/view/src/main/java/shokoku/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/shokoku/board/view/service/ArticleViewService.java
@@ -3,6 +3,9 @@ package shokoku.board.view.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import shokoku.board.view.repository.ArticleViewCountRepository;
+import shokoku.board.view.repository.ArticleViewDistributedLockRepository;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
@@ -10,9 +13,15 @@ public class ArticleViewService {
 
   private final ArticleViewCountRepository articleViewCountRepository;
   private final ArticleViewCountBackupProcessor articleViewCountBackupProcessor;
+  private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
   private static final int BACK_UP_BACH_SIZE = 100;
+  private static final Duration TTL = Duration.ofMinutes(10);
 
   public Long increase(Long articleId, Long userId) {
+    if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+      return count(articleId);
+    }
+
     Long count = articleViewCountRepository.increase(articleId);
     if (count % BACK_UP_BACH_SIZE == 0) {
       articleViewCountBackupProcessor.backup(articleId, count);
