d301f8b8cb9a0d4c42857cc140fcfab81e97480d
hyeonggyu-kim <shokoku0612@gmail.com>
2025-11-30 14:49:20 +0900

    feat: Implementing a Popular Posts Repository

---
+++ service/hot-article/src/test/java/shokoku/board/hotarticle/repository/HotArticleListRepositoryTest.java
@@ -0,0 +1,45 @@
+package shokoku.board.hotarticle.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HotArticleListRepositoryTest {
+
+  @Autowired
+  HotArticleListRepository hotArticleListRepository;
+
+  @Test
+  void addTest() throws InterruptedException {
+
+    LocalDateTime time = LocalDateTime.of(2025, 11, 30, 0, 0);
+    long limit = 3;
+
+    hotArticleListRepository.add(1L, time, 2L, limit, Duration.ofSeconds(3));
+    hotArticleListRepository.add(2L, time, 3L, limit, Duration.ofSeconds(3));
+    hotArticleListRepository.add(3L, time, 1L, limit, Duration.ofSeconds(3));
+    hotArticleListRepository.add(4L, time, 5L, limit, Duration.ofSeconds(3));
+    hotArticleListRepository.add(5L, time, 4L, limit, Duration.ofSeconds(3));
+
+    List<Long> articleIds = hotArticleListRepository.readAll("20251130");
+
+    assertThat(articleIds).hasSize(Long.valueOf(limit).intValue());
+    assertThat(articleIds.get(0)).isEqualTo(4);
+    assertThat(articleIds.get(1)).isEqualTo(5);
+    assertThat(articleIds.get(2)).isEqualTo(2);
+
+    TimeUnit.SECONDS.sleep(5);
+    assertThat(hotArticleListRepository.readAll("20251130")).isEmpty();
+
+  }
+}
