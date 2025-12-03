0c263d3c087e4cecd7003215167cb7308f4a653b
hyeonggyu-kim <shokoku0612@gmail.com>
2025-12-03 13:49:07 +0900

    feat: Implementation of Post List Repository Optimization Strategy

---
+++ service/article-read/src/main/java/shokoku/board/articleread/repository/BoardArticleCountRepository.java
@@ -0,0 +1,27 @@
+package shokoku.board.articleread.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardArticleCountRepository {
+  private final StringRedisTemplate redisTemplate;
+
+  // article-read::board::article-count::board::{boardId}
+  private static final String KEY_FORMAT = "article-read::board::article-count::board::%s";
+
+  public void createOrUpdate(Long boardId, Long articleCount) {
+    redisTemplate.opsForValue().set(GenerateKey(boardId), String.valueOf(articleCount));
+  }
+
+  public Long read(Long boardId) {
+    String result = redisTemplate.opsForValue().get(GenerateKey(boardId));
+    return result == null ? 0L : Long.parseLong(result);
+  }
+
+  private String GenerateKey(Long boardId) {
+    return KEY_FORMAT.formatted(boardId);
+  }
+}
